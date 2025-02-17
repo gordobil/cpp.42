928b4bb268effdcf07c1bf2bebb97a3f710c6a54
gordobil <noahgordobil@gmail.com>
2025-02-17 22:11:47 +0100

    ex00 done

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -1,6 +1,6 @@
 /* ************************************************************************** */
-/*                                                                            */
 /*                                                        :::      ::::::::   */
+/*                                                                            */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngordobi <ngordobi@student.42urduliz.co    +#+  +:+       +#+        */
@@ -14,7 +14,23 @@
 
 int main(int argc, char **argv)
 {
-	argc++;
-	argv++;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEAREBLE FEEDBACK NOISE *" << std::endl;
+		return (1);
+	}
+
+	for (int i = 1; i < argc; i++)
+	{
+		for (int j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
+				argv[i][j] = argv[i][j] - 32;
+			std::cout << argv[i][j];
+		}
+		std::cout << ' ';
+	}
+	std::cout <<std::endl;
+
 	return (0);
 }
