ef1ef17da44bf1eaa042db86a59d37cfcc75f63d
Noah Gordobil Merino <ngordobi@c3r6s6.42urduliz.com>
2025-03-03 13:20:24 +0100

    m2

---
+++ module_02/ex00/Fixed.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngordobi <ngordobi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/03 13:19:42 by ngordobi          #+#    #+#             */
+/*   Updated: 2025/03/03 13:19:42 by ngordobi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	FIXED_HPP
+#define	FIXED_HPP
+
+#include <iostream>
+#include <string>
+
+# define RED	"\033[1;95m"
+# define CYAN	"\033[1;31m"
+# define WHITE	"\033[0m"
+
+class Fixed
+{
+	private:
+
+	public:
+		Fixed();
+		~Fixed();
+};
+
+#endif
