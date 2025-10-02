bde314046f2232b95de71a9b682e86ead190261b
gordobil <noahgordobil@gmail.com>
2025-10-02 16:23:32 +0200

    m08 ex00 done

---
+++ module_08/ex00/easyfind.hpp
@@ -0,0 +1,19 @@
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <deque>
+
+template <typename T>
+void	easyfind(T &container, int n){
+	if (std::find(container.begin(), container.end(), n) == container.end())
+		std::cout << n << " not found." << std::endl;
+	else
+		std::cout << n << " found."<< std::endl;
+}
+
+#endif
---
+++ module_08/ex00/main.cpp
@@ -0,0 +1,47 @@
+
+#include "easyfind.hpp"
+
+int	main(void)
+{
+	std::vector<int>	v;
+	std::deque<int>		d;
+	std::list<int>		l;
+	std::deque<char>	c;
+
+	std::cout << " ¬ VECTOR:" << std::endl;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+	easyfind(v, 4);
+	std::cout << std::endl;
+
+	std::cout << " ¬ DEQUE:" << std::endl;
+	d.push_back(13);
+	d.push_back(348);
+	d.push_back(38927);
+	d.push_back(9993939);
+	easyfind(d, 38927);
+	std::cout << std::endl;
+
+	std::cout << " ¬ LIST:" << std::endl;
+	l.push_back(200);
+	l.push_back(100);
+	l.push_back(50);
+	l.push_back(25);
+	l.push_back(5);
+	l.push_back(1);
+	easyfind(l, 50);
+	std::cout << std::endl;
+
+	std::cout << "* NOT FOUND:" << std::endl;
+	easyfind(v, 4);
+	std::cout << std::endl;
+
+	std::cout << "* CHAR:" << std::endl;
+	c.push_back('A');
+	c.push_back('B');
+	c.push_back('C');
+	easyfind(c, 'B');
+
+	return (0);
+}
