17c8243604a88cf43bf4dad0f4b80be1a4374c1b
gordobil <noahgordobil@gmail.com>
2025-10-23 13:56:42 +0200

    add main_intra

--- module_07/ex00/main.cpp
+++ module_07/ex00/main.cpp
@@ -1,33 +1,66 @@
 
 #include "whatever.tpp"
 
-int	main(void)
+class Awesome
 {
-	int			n1 = 5;
-	int			n2 = 8;
-	std::string	s1 = "string1";
-	std::string	s2 = "string2";
+	private:
+		int		_n;
 
-	std::cout << RED "	INTS" CYAN << std::endl;
-	std::cout << "n1: " WHITE << n1 << CYAN " / n2: " WHITE << n2 << std::endl;
-	::swap(n1, n2);
-	std::cout << CYAN "swapped -> n1: " WHITE << n1 << CYAN " / n2: " WHITE << n2 << std::endl;
-	std::cout << CYAN "min: " WHITE << ::min(n1, n2) << std::endl;
-	std::cout << CYAN "max: " WHITE << ::max(n1, n2) << std::endl << std::endl;
+	public:
+		Awesome(void) : _n(0) {}
+		Awesome(int n) : _n(n) {}
+		Awesome	&operator=(Awesome &a) {_n = a._n; return (*this);}
+		bool	operator==(Awesome &rhs)const {return (this->_n == rhs._n);}
+		bool	operator!=(Awesome &rhs)const {return (this->_n != rhs._n);}
+		bool	operator>(Awesome &rhs)const {return (this->_n > rhs._n);}
+		bool	operator<(Awesome &rhs)const {return (this->_n < rhs._n);}
+		bool	operator>=(Awesome &rhs)const {return (this->_n >= rhs._n);}
+		bool	operator<=(Awesome &rhs)const {return (this->_n <= rhs._n);}
+		int		get_n(void)const {return (_n);}
 
-	std::cout << RED "	STRINGS" CYAN << std::endl;
-	std::cout << "s1: " WHITE << s1 << CYAN " / s2: " WHITE << s2 << std::endl;
-	::swap(s1, s2);
-	std::cout << CYAN "swapped -> s1: " WHITE << s1 << CYAN " / s2: " WHITE << s2 << std::endl;
-	std::cout << CYAN "min: " WHITE << ::min(s1, s2) << std::endl;
-	std::cout << CYAN "max: " WHITE << ::max(s1, s2) << std::endl << std::endl;
+};
+std::ostream	&operator<<(std::ostream &o, const Awesome &a) {o << a.get_n(); return (o);}
 
-	std::cout << RED "	CHARS" CYAN << std::endl;
-	std::cout << "c1: " WHITE << s1[6] << CYAN " / c2: " WHITE << s2[6] << std::endl;
-	::swap(s1[6], s2[6]);
-	std::cout << CYAN "swapped -> c1: " WHITE << s1[6] << CYAN " / c2: " WHITE << s2[6] << std::endl;
-	std::cout << CYAN "min: " WHITE << ::min(s1[6], s2[6]) << std::endl;
-	std::cout << CYAN "max: " WHITE << ::max(s1[6], s2[6]) << std::endl;
+void	intra_main(void){
+	Awesome	a(2), b(4);
+
+	swap (a, b);
+	std::cout << a << " " << b << std::endl;
+	std::cout << max(a, b) << std::endl;
+	std::cout << min(a, b) << std::endl;
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2 && (std::string)argv[1] == "intra")
+		intra_main();
+	else {
+		int			n1 = 5;
+		int			n2 = 8;
+		std::string	s1 = "string1";
+		std::string	s2 = "string2";
+
+		std::cout << RED "	INTS" CYAN << std::endl;
+		std::cout << "n1: " WHITE << n1 << CYAN " / n2: " WHITE << n2 << std::endl;
+		::swap(n1, n2);
+		std::cout << CYAN "swapped -> n1: " WHITE << n1 << CYAN " / n2: " WHITE << n2 << std::endl;
+		std::cout << CYAN "min: " WHITE << ::min(n1, n2) << std::endl;
+		std::cout << CYAN "max: " WHITE << ::max(n1, n2) << std::endl << std::endl;
+
+		std::cout << RED "	STRINGS" CYAN << std::endl;
+		std::cout << "s1: " WHITE << s1 << CYAN " / s2: " WHITE << s2 << std::endl;
+		::swap(s1, s2);
+		std::cout << CYAN "swapped -> s1: " WHITE << s1 << CYAN " / s2: " WHITE << s2 << std::endl;
+		std::cout << CYAN "min: " WHITE << ::min(s1, s2) << std::endl;
+		std::cout << CYAN "max: " WHITE << ::max(s1, s2) << std::endl << std::endl;
+
+		std::cout << RED "	CHARS" CYAN << std::endl;
+		std::cout << "c1: " WHITE << s1[6] << CYAN " / c2: " WHITE << s2[6] << std::endl;
+		::swap(s1[6], s2[6]);
+		std::cout << CYAN "swapped -> c1: " WHITE << s1[6] << CYAN " / c2: " WHITE << s2[6] << std::endl;
+		std::cout << CYAN "min: " WHITE << ::min(s1[6], s2[6]) << std::endl;
+		std::cout << CYAN "max: " WHITE << ::max(s1[6], s2[6]) << std::endl;
+	}
 
 	return (6);
 }
