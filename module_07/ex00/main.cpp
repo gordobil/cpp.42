ecb669f9408725ab2f34599211bdecc7f80e931d
gordobil <noahgordobil@gmail.com>
2025-10-23 14:22:08 +0200

    m07 mains

--- module_07/ex00/main.cpp
+++ module_07/ex00/main.cpp
@@ -21,46 +21,47 @@ class Awesome
 };
 std::ostream	&operator<<(std::ostream &o, const Awesome &a) {o << a.get_n(); return (o);}
 
-void	complex(void){
+int	complex(void){
 	Awesome	a(2), b(4);
 
 	swap (a, b);
 	std::cout << a << " " << b << std::endl;
 	std::cout << max(a, b) << std::endl;
 	std::cout << min(a, b) << std::endl;
+
+	return (0);
 }
 
 int	main(int argc, char **argv)
 {
 	if (argc == 2 && (std::string)argv[1] == "complex")
-		complex();
-	else {
-		int			n1 = 5;
-		int			n2 = 8;
-		std::string	s1 = "string1";
-		std::string	s2 = "string2";
+		return (complex());
 
-		std::cout << RED "	INTS" CYAN << std::endl;
-		std::cout << "n1: " WHITE << n1 << CYAN " / n2: " WHITE << n2 << std::endl;
-		::swap(n1, n2);
-		std::cout << CYAN "swapped -> n1: " WHITE << n1 << CYAN " / n2: " WHITE << n2 << std::endl;
-		std::cout << CYAN "min: " WHITE << ::min(n1, n2) << std::endl;
-		std::cout << CYAN "max: " WHITE << ::max(n1, n2) << std::endl << std::endl;
+	int			n1 = 5;
+	int			n2 = 8;
+	std::string	s1 = "string1";
+	std::string	s2 = "string2";
 
-		std::cout << RED "	STRINGS" CYAN << std::endl;
-		std::cout << "s1: " WHITE << s1 << CYAN " / s2: " WHITE << s2 << std::endl;
-		::swap(s1, s2);
-		std::cout << CYAN "swapped -> s1: " WHITE << s1 << CYAN " / s2: " WHITE << s2 << std::endl;
-		std::cout << CYAN "min: " WHITE << ::min(s1, s2) << std::endl;
-		std::cout << CYAN "max: " WHITE << ::max(s1, s2) << std::endl << std::endl;
+	std::cout << RED "	INTS" CYAN << std::endl;
+	std::cout << "n1: " WHITE << n1 << CYAN " / n2: " WHITE << n2 << std::endl;
+	::swap(n1, n2);
+	std::cout << CYAN "swapped -> n1: " WHITE << n1 << CYAN " / n2: " WHITE << n2 << std::endl;
+	std::cout << CYAN "min: " WHITE << ::min(n1, n2) << std::endl;
+	std::cout << CYAN "max: " WHITE << ::max(n1, n2) << std::endl << std::endl;
 
-		std::cout << RED "	CHARS" CYAN << std::endl;
-		std::cout << "c1: " WHITE << s1[6] << CYAN " / c2: " WHITE << s2[6] << std::endl;
-		::swap(s1[6], s2[6]);
-		std::cout << CYAN "swapped -> c1: " WHITE << s1[6] << CYAN " / c2: " WHITE << s2[6] << std::endl;
-		std::cout << CYAN "min: " WHITE << ::min(s1[6], s2[6]) << std::endl;
-		std::cout << CYAN "max: " WHITE << ::max(s1[6], s2[6]) << std::endl;
-	}
+	std::cout << RED "	STRINGS" CYAN << std::endl;
+	std::cout << "s1: " WHITE << s1 << CYAN " / s2: " WHITE << s2 << std::endl;
+	::swap(s1, s2);
+	std::cout << CYAN "swapped -> s1: " WHITE << s1 << CYAN " / s2: " WHITE << s2 << std::endl;
+	std::cout << CYAN "min: " WHITE << ::min(s1, s2) << std::endl;
+	std::cout << CYAN "max: " WHITE << ::max(s1, s2) << std::endl << std::endl;
+
+	std::cout << RED "	CHARS" CYAN << std::endl;
+	std::cout << "c1: " WHITE << s1[6] << CYAN " / c2: " WHITE << s2[6] << std::endl;
+	::swap(s1[6], s2[6]);
+	std::cout << CYAN "swapped -> c1: " WHITE << s1[6] << CYAN " / c2: " WHITE << s2[6] << std::endl;
+	std::cout << CYAN "min: " WHITE << ::min(s1[6], s2[6]) << std::endl;
+	std::cout << CYAN "max: " WHITE << ::max(s1[6], s2[6]) << std::endl;
 
 	return (6);
 }
--- module_07/ex01/main.cpp
+++ module_07/ex01/main.cpp
@@ -15,34 +15,35 @@ std::ostream	&operator<<(std::ostream &o, const Awesome &rhs) {o << rhs.get(); r
 template <typename T>
 void	print(const T &x) {std::cout << x << std::endl; return ;}
 
-void	complex(void){
+int	complex(void){
 	int		tab[] = {0, 1, 2, 3, 4};
 	Awesome	tab2[5];
 
 	iter(tab, 5, print);
 	iter(tab2, 5, print);
+
+	return (0);
 }
 
 int	main(int argc, char **argv)
 {
 	if (argc == 2 && (std::string)argv[1] == "complex")
-		complex();
-	else {
-		int			n[] = {1, 2, 3, 4, 5};
-		std::string	s[]	= {"one", "two", "three", "four", "five"};
-		char		c[] = {'a', 'b', 'c', 'd', 'e'};
+		return (complex());
 
-		std::cout << RED "INT:" WHITE << std::endl;
-		::iter(n, 5, print_values);
-		std::cout << std::endl;
+	int			n[] = {1, 2, 3, 4, 5};
+	std::string	s[]	= {"one", "two", "three", "four", "five"};
+	char		c[] = {'a', 'b', 'c', 'd', 'e'};
 
-		std::cout << RED "STRING:" WHITE << std::endl;
-		::iter(s, 5, print_values);
-		std::cout << std::endl;
+	std::cout << RED "INT:" WHITE << std::endl;
+	::iter(n, 5, print_values);
+	std::cout << std::endl;
 
-		std::cout << RED "CHAR:" WHITE << std::endl;
-		::iter(c, 5, print_values);
-	}
+	std::cout << RED "STRING:" WHITE << std::endl;
+	::iter(s, 5, print_values);
+	std::cout << std::endl;
+
+	std::cout << RED "CHAR:" WHITE << std::endl;
+	::iter(c, 5, print_values);
 
 	return (0);
 }
