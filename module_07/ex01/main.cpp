6601d05110880c082e76077f65acc17a1146d8c7
gordobil <noahgordobil@gmail.com>
2025-10-23 14:17:48 +0200

    add main_intra m07ex01

--- module_07/ex01/main.cpp
+++ module_07/ex01/main.cpp
@@ -1,20 +1,48 @@
 
 #include "iter.tpp"
 
-int	main(void)
+class Awesome
 {
-	int			n[] = {1, 2, 3, 4, 5};
-	std::string	s[]	= {"one", "two", "three", "four", "five"};
-	char		c[] = {'a', 'b', 'c', 'd', 'e'};
+	private:
+		int _n;
 
-	std::cout << RED "INT:" WHITE << std::endl;
-	::iter(n, 5, print_values);
-	std::cout << std::endl;
+	public:
+		Awesome(void) : _n(42) {return ;}
+		int	get(void)const {return (this->_n);}
+};
+std::ostream	&operator<<(std::ostream &o, const Awesome &rhs) {o << rhs.get(); return (o);}
 
-	std::cout << RED "STRING:" WHITE << std::endl;
-	::iter(s, 5, print_values);
-	std::cout << std::endl;
+template <typename T>
+void	print(const T &x) {std::cout << x << std::endl; return ;}
 
-	std::cout << RED "CHAR:" WHITE << std::endl;
-	::iter(c, 5, print_values);
+void	complex(void){
+	int		tab[] = {0, 1, 2, 3, 4};
+	Awesome	tab2[5];
+
+	iter(tab, 5, print);
+	iter(tab2, 5, print);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2 && (std::string)argv[1] == "complex")
+		complex();
+	else {
+		int			n[] = {1, 2, 3, 4, 5};
+		std::string	s[]	= {"one", "two", "three", "four", "five"};
+		char		c[] = {'a', 'b', 'c', 'd', 'e'};
+
+		std::cout << RED "INT:" WHITE << std::endl;
+		::iter(n, 5, print_values);
+		std::cout << std::endl;
+
+		std::cout << RED "STRING:" WHITE << std::endl;
+		::iter(s, 5, print_values);
+		std::cout << std::endl;
+
+		std::cout << RED "CHAR:" WHITE << std::endl;
+		::iter(c, 5, print_values);
+	}
+
+	return (0);
 }
